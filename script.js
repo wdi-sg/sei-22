a140720872dc9539c9cfa4f0b9c5b7bbfb1645e3
Ari <ari@Aris-MacBook-Air.local>
2020-03-30 13:42:14 +0800

    game logic created, Still need to improve on the logic and the styling

---
+++ script.js
@@ -0,0 +1,113 @@
+//button.addEventListener("click", function(){
+//	if (currentInput === '') {
+//		alert("Please enter your name");
+
+//	else {
+//		return initiate (currentInput);
+//	}
+//});
+
+var display = function( data ){
+	var displayElement = document.querySelector('#output');
+	displayElement.innerHTML = "";
+    output.innerText = data;
+    };
+
+document.querySelector('#input').addEventListener('change', function(event){
+	var currentInput = event.target.value;
+    var result = inputHappened(currentInput);
+    display( result );
+    });
+
+var footballersData = [
+	{
+	name: 'thierry henry' , 
+	description: 'I started my career in Frace with AS Monaco. But earned my fame playing in North London for Arsenal in the early 2000s. Subsequently I had 2 final stints in Barcelone and in Major League Soccer', 
+	imgSource: 'to be entered' 
+	},
+	{
+	name: 'david beckham' , 
+	description: 'I have received much following on and off the pitch. I started off playing for my boyhood club under Alex Ferguson. However after a spat, I moved to Real Madrid', 
+	imgSource: 'to be entered' 
+	},
+	{
+	name: 'lionel messi' , 
+	description: 'I have been in Barcelona my whole career. Many people call me the goat!', 
+	imgSource: 'to be entered' 
+	}
+];
+//var bailoutCard = footballersData[].imgSource;
+
+console.log(footballersData);
+
+var attemptNumber = 0;
+var attemptsLeft = 3;
+var secretWord = [];
+var guessedLetters = [];
+var turns = 0;
+
+var initiate = function(currentInput) {
+	var name = currentInput;
+	turns ++;
+	return "Hello " + name + " Are you ready to test your football players knowledge? For each level, you will be given 4 chances to guess the letter. If you fail to guess the player in 4 attempts you fail. Fret not, you are also given 3 bailout cards which will show you a photo of the player."
+
+}
+var generateQueston = function() {
+	var randomIndex = Math.floor(Math.random()*3);
+	console.log(randomIndex);
+	secretWord = footballersData[randomIndex].name.split("");
+	console.log(secretWord);
+	turns ++;
+	return footballersData[randomIndex].description;
+}
+
+
+var checkWithInput = function(currentInput) {
+  
+  for (var i = 0; i < secretWord.length; i++){
+    if (secretWord[i] === currentInput) {
+      answer = true;
+      return answer;
+    }
+    else {
+      var answer = false;
+    }
+  }
+  return answer;
+  
+}
+
+var inputHappened = function(currentInput){
+  console.log( currentInput );
+  if (turns === 0) {
+  	return initiate(currentInput);
+  }
+  if (turns === 1) {
+  	return generateQueston(currentInput);
+  }
+  else {
+  	if (checkWithInput(currentInput) === false && attemptsLeft === 0){
+    	return "Game Over";
+  	}		
+  	else if (checkWithInput(currentInput) === false && attemptsLeft > 0){
+    	attemptNumber++;
+    	attemptsLeft = (attemptsLeft - attemptNumber);
+    	console.log(attemptsLeft);
+    	return "you guessed wrong, you have " + attemptsLeft + " tries left";
+
+  	} 
+    else if (checkWithInput(currentInput) === true) {
+    	guessedLetters.push(currentInput);
+      	console.log(guessedLetters)
+      	if (guessedLetters.length === secretWord.length) {
+      		turns = 1;
+        	return "Well done, you got it! Ready for the next question?"
+      }
+    
+      else {
+        return "You guessed right! Guess the next letter";
+      }
+    }
+}
+}
+    
